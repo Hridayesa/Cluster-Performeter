e0b744cb710cf3ee50f70dc7c388b05e75f87f75
Hridayesa <denis.karpoff@gmail.com>
2016-12-05 11:23:26 +0300

    redis

--- cluster-performeter-hazlecast/src/main/java/org/vs/performeter/hzcache/orchestrator/HazelcastCacheTestAnalyser.java
+++ cluster-performeter-hazlecast/src/main/java/org/vs/performeter/hzcache/orchestrator/HazelcastCacheTestAnalyser.java
@@ -1,29 +1,19 @@
 package org.vs.performeter.hzcache.orchestrator;
 
-import com.hazelcast.core.IMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.vs.performeter.core.common.Analyser;
-
-import javax.annotation.Resource;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * Created by karpovdc on 07.09.2015.
  */
-@Component
-public class HazelcastCacheTestAnalyser implements Analyser{
-    private static Logger LOG = LoggerFactory.getLogger(HazelcastCacheTestAnalyser.class);
-
-    @Resource private IMap statisticsMap;
-
-    public void analyse(){
-        LOG.info("## statisticsMap.size()="+statisticsMap.size());
-        ((Set<Map.Entry>)statisticsMap.entrySet()).forEach(
-                entry -> LOG.info("### "+entry.getKey() + "/" + entry.getValue())
-        );
-    }
-
-}
+//@Component
+//public class HazelcastCacheTestAnalyser implements Analyser{
+//    private static Logger LOG = LoggerFactory.getLogger(HazelcastCacheTestAnalyser.class);
+//
+//    @Resource private IMap statisticsMap;
+//
+//    public void analyse(){
+//        LOG.info("## statisticsMap.size()="+statisticsMap.size());
+//        ((Set<Map.Entry>)statisticsMap.entrySet()).forEach(
+//                entry -> LOG.info("### "+entry.getKey() + "/" + entry.getValue())
+//        );
+//    }
+//
+//}
--- cluster-performeter-redis/src/test/java/org/vs/performeter/redis/RedisTest1.java
+++ cluster-performeter-redis/src/test/java/org/vs/performeter/redis/RedisTest1.java
@@ -3,12 +3,14 @@ package org.vs.performeter.redis;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import javax.annotation.Resource;
 
 /**
  * Created by dekar on 02.12.2016.
@@ -17,12 +19,13 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @ContextConfiguration(classes = {RedisConfig.class}, loader = AnnotationConfigContextLoader.class)
 @TestPropertySource( locations = {"classpath:app.properties"}, properties = {"isTesterInstance = true"})
 public class RedisTest1 {
-    @Autowired
-    StringRedisTemplate template;
+    @Resource(name = "redisTemplate")
+    RedisTemplate template;
 
     @Test
     public void testRedis1() throws Exception {
-        template.opsForHash().put("WWW","fld1","value 1");
-        Assert.assertEquals("value 1", template.opsForHash().get("WWW", "fld1"));
+        HashOperations<String, String, String> hashOperations = template.opsForHash();
+        hashOperations.put("WWW","fld1","value 1");
+        Assert.assertEquals("value 1", hashOperations.get("WWW", "fld1"));
     }
 }
